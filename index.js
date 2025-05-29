037cbc3bcd9938dd397550a996b06f11629e5113
AvelileM <aveliled@gmail.com>
2025-05-29 10:41:25 +0200

    updated newfile

--- index.js
+++ index.js
@@ -1,7 +1,109 @@
-let x = 20
-let y = 30
+console.log("\n")
+// let x = 20
+// let y = 25
+// let z = 30
 
-if(x<30){
-    console.log("x is less than", y)
+// if(x<30){
+    //     console.log("x is less than", y)
+    
+    // }
+    
+    // else if(z>y){
+        //     console.log("z is bigger than", y)
+        // } 
+        
+        // else{
+            //     console.log("x is less than", z)
+            // }
+            
+            
+            // switch(x){
+//     case 25:
+//         console.log("Number is", x)
+//         break
+//     default:
+//         console.log("Number does not match")
+//         break  
+// }
 
+// switch(true){
+    //     case x == 20:
+    //         console.log("Number is", x)
+    //         break
+    
+    //     case 25:
+    //         console.log("Number is", x)
+    //         break
+    
+    //     default:
+    //         console.log("Number does not match")
+    //         break
+    
+// }
+
+// // Ternary Operator
+// console.log(x<y? "True":"False")
+// console.log(x<y? x-=1:x+=1)
+
+// let y = 15
+// for (let x = 0; x < 10; x+=1){
+//     console.log(x)
+//     console.log(x>y? x-=1:x+=1)
+//     console.log("\n")
+    
+// }
+
+// x = 0
+// while (x<=y){
+//     console.log(x)
+//     x++
+// }
+
+
+// do {
+//     console.log(x)
+//     x++
+    
+// } while (x<=y);
+
+
+let fruits = ["Pear","Banana","Aplle","Strawberry","Pineapple","Guava","Orange"]
+
+for (let x of fruits){
+    console.log(x)
 }
+
+console.log("\n")
+
+for (let x in fruits){
+    console.log(x)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("\n")
